e6091003eccbca05cee643ebf0e8667832550a24
MatheusBorsa <matheusalek485@icloud.com>
2025-03-10 15:37:43 -0700

    feat: implement logout function

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
 
 Route::get('/test', function () {
     return response('test');
