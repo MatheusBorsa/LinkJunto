d2cf417edc212afab33f378a7760c754e9bcccfd
MatheusBorsa <matheusalek485@icloud.com>
2025-03-10 21:17:07 -0700

    feat: updating routess for profile controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,8 +11,16 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
-Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
 
-Route::get('/test', function () {
-    return response('test');
+Route::middleware('auth:sanctum')->group(function() {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::prefix('/profile')->group(function() {
+        Route::put('/update', [ProfileController::class, 'updateUser']);
+        Route::post('/links', [ProfileController::class, 'addLink']);
+        Route::put('/links/{id}/order', [ProfileController::class, 'updateLinkOrder']);
+        Route::delete('/links/{id}', [ProfileController::class, 'deleteLink']);
+        Route::get('/', [ProfileController::class, 'show']);
+    });
 });
