007c311ec66435adfb0ae764345d2bbf47bcc540
MatheusBorsa <Matheusalek485@icloud.com>
2025-03-14 11:38:10 -0700

    feat: test for creating link endpoint

---
+++ tests/Feature/Auth/CreateLinkTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Tests\ApiTestCase;
+use App\Models\User;
+
+class CreateLinkTest extends ApiTestCase
+{
+    public function test_creating_valid_link()
+    {
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $data = [
+            'title' => 'Example Title',
+            'url' => 'https://example.com',
+            'order' => 1
+        ];
+
+        $response = $this->postJson('/api/profile/links', $data);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            'title' => 'Example Title',
+            'url' => 'https://example.com',
+            'order' => 1
+        ]);
+    }
+
+    public function test_creating_invalid_link()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $data = [
+            'title' => '',
+            'url' => 'Not a Valid url',
+            'order' => 'Not a Integer'
+        ];
+
+        $response = $this->postJson('/api/profile/links', $data);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonValidationErrors(['title', 'url', 'order']);
+    }
+
+    public function test_creating_link_unauthenticated()
+    {
+        $data = [
+            'title' => 'Example title',
+            'url' => 'https://example.com',
+            'order' => 1
+        ];
+
+        $response = $this->postJson('api/profile/links', $data);
+
+        $response->assertStatus(401);
+    }
+}
